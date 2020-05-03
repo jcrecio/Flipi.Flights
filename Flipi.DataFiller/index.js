a58f8e3eb648efc67312f06370b9f1eaeae32119
juanky <juan.carlos.recio.abad@gmail.com>
2020-05-03 12:58:58 +0200

    Consuming topic messages

--- Flipi.DataFiller/index.js
+++ Flipi.DataFiller/index.js
@@ -1,13 +1,23 @@
 const kafka = require('kafka-node');
-// const bp = require('body-parser');
 const config = require('./config');
 
 try {
-  const Consumer = kafka.Consumer;
-  const client = new kafka.KafkaClient(config.kafka_server);
-  let consumer = new Consumer(
+  var kafkaClientOptions = {
+    clientId: 'kafkafiller',
+    kafkaHost : config.kafka_server,//,broker2:9092,broker3:9092',
+    autoConnect: true,
+    connectTimeout: 5000,
+    requestTimeout: 5000
+  };
+  
+  const client = new kafka.KafkaClient(kafkaClientOptions);
+  let consumer = new kafka.Consumer(
     client,
-    [{ topic: config.kafka_topic, partition: 0 }],
+    [
+      {
+        topic: kafka_server.kafka_topic
+      }
+    ],
     {
       autoCommit: true,
       fetchMaxWaitMs: 1000,
@@ -16,17 +26,13 @@ try {
       fromOffset: false
     }
   );
-  consumer.on('message', async function(message) {
-    console.log('here');
-    console.log(
-      'kafka-> ',
-      message.value
-    );
+  consumer.on('message', async function (message) {
+    console.log('kafka-> ', message.value);
   })
-  consumer.on('error', function(err) {
+  consumer.on('error', function (err) {
     console.log('error', err);
   });
 }
-catch(e) {
+catch (e) {
   console.log(e);
 }
